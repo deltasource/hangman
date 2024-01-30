247a4ce95e363bffb60037fcc70c839cdaa2db50
AnassAfqirBouchnafat <AnassAfqirBouchnafat@192.168.0.180>
2024-01-30 10:03:52 +0100

    hangman - while loop letters

--- src/main/java/eu/deltasource/hangman/Hangman.java
+++ src/main/java/eu/deltasource/hangman/Hangman.java
@@ -1,9 +1,21 @@
 package eu.deltasource.hangman;
 
+import java.util.Scanner;
 
 public class Hangman {
 
 	public static void main(String[] args) {
-		System.out.println("Replace me");
+		Scanner scanner = new Scanner(System.in);
+		
+		String letter = "";
+		
+		while(!letter.equalsIgnoreCase("-")) {
+			System.out.println("geef een letter in");
+			letter = scanner.next();
+			System.out.println(letter);
+		}
+		System.out.println("while loop is gestopt");
+		
+		
 	}
 }
