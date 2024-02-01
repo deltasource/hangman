5b912e0eb7ed94c725c28d4e93074e0ad967b261
AnassAfqirBouchnafat <AnassAfqirBouchnafat@192.168.0.180>
2024-02-01 15:56:55 +0100

    fourth version hangman (nog niet klaar)

--- src/main/java/eu/deltasource/hangman/Hangman.java
+++ src/main/java/eu/deltasource/hangman/Hangman.java
@@ -4,18 +4,64 @@ import java.util.Scanner;
 
 public class Hangman {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		String letter = "";
-		
-		while(!letter.equalsIgnoreCase("-")) {
-			System.out.println("geef een letter in");
-			letter = scanner.next();
-			System.out.println(letter);
-		}
-		System.out.println("while loop is gestopt");
-		
-		
+    private int maxErrors;
+    private int error;
+
+    public Hangman() {
+	setMaxErrors(6);
+	error = 0;
+    }
+
+    public static void main(String[] args) {
+	Hangman hangman = new Hangman();
+	HangmanWordGuess hangmanwordguess = new HangmanWordGuess();
+	System.out.println(hangmanwordguess.getWord());
+	Scanner scanner = new Scanner(System.in);
+	Character character = null;
+	String letter;
+
+	while (!hangmanwordguess.getGuessedWord().equals(hangmanwordguess.getWord())
+		&& hangman.getError() != hangman.getMaxErrors()) {
+	    System.out.println("Pick a letter");
+	    letter = scanner.next();
+
+	    if (letter.length() == 1) {
+		character = letter.charAt(0);
+	    } else {
+		System.out.println("only one lettre please");
+		continue;
+	    }
+
+	    if (hangmanwordguess.guessCharacter(character) == false) {
+		hangman.setError(hangman.getError() + 1);
+	    }
+
+	    System.out.println("Guessed Word: " + hangmanwordguess.getGuessedWord());
+	    System.out.println();
+	    System.out.println("Available Characters: " + hangmanwordguess.getAvailableCharacters());
 	}
+
+	if (hangman.getError() == hangman.getMaxErrors()) {
+	    System.out.println("You lose The word was: " + hangmanwordguess.getWord());
+	} else {
+	    System.out.println("You win");
+	}
+
+    }
+
+    public int getError() {
+	return error;
+    }
+
+    public void setError(int error) {
+	this.error = error;
+    }
+
+    public int getMaxErrors() {
+	return maxErrors;
+    }
+
+    public void setMaxErrors(int maxErrors) {
+	this.maxErrors = maxErrors;
+    }
 }
