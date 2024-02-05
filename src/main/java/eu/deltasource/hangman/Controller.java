9989345899b7960e4205e44a30ffca6a13fa8a50
AnassAfqirBouchnafat <AnassAfqirBouchnafat@170-5CG1362GTX>
2024-02-05 14:11:36 +0100

    created frontend in javaFX

--- src/main/java/eu/deltasource/hangman/Controller.java
+++ src/main/java/eu/deltasource/hangman/Controller.java
@@ -1,5 +1,6 @@
 package eu.deltasource.hangman;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -14,74 +15,85 @@ public class Controller {
     public HangmanWordGuess hangmanwordguess;
     public ImageView printGallow;
     public Label availableChar;
-    public int teller =0;
-    
+    public Button restart;
+    public int teller = 0;
+
     public void initialize() {
 	hangman = new Hangman();
 	hangmanwordguess = new HangmanWordGuess();
 	printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_1.png"));
 	geussedWord.setText(hangmanwordguess.getGuessedWord());
 	availableChar.setText(hangmanwordguess.getAvailableCharacters());
+	restart.setVisible(false);
     }
-    
-    public void Guess() {
-	    Character character = null;
-	    	if(teller == 0) {
-	    	    
+
+    public void guess() {
+	Character character = null;
+	if (teller == 0) {
+
 	    if (letter.getText().length() == 1) {
 		character = letter.getText().charAt(0);
-	    } else {
+	    } else{
 		print.setText("only one letter please");
 	    }
-	    
+
 	    if (hangmanwordguess.guessCharacter(character) == false) {
 		hangman.setError(hangman.getError() + 1);
 	    }
+	    
 	    availableChar.setText(hangmanwordguess.getAvailableCharacters());
 	    geussedWord.setText(hangmanwordguess.getGuessedWord());
-	   // print.setText(String.valueOf(hangman.getError()));
 	    letter.clear();
-	    
-	    if(hangman.getError() == 0) {
+
+	    switch (hangman.getError()) {
+	    case 0:
 		printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_1.png"));
-	    }
-	    else if(hangman.getError() == 1) {
+		break;
+	    case 1:
 		printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_2.png"));
-	    }
-	    else if(hangman.getError() == 2) {
+		break;
+	    case 2:
 		printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_3.png"));
-	    }
-	    else if(hangman.getError() == 3) {
+		break;
+	    case 3:
 		printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_4.png"));
-	    }
-	    else if(hangman.getError() == 4) {
+		break;
+	    case 4:
 		printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_5.png"));
-	    }
-	    else if(hangman.getError() == 5) {
+		break;
+	    case 5:
 		printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_6.png"));
-	    }
-	    else if(hangman.getError() == 6) {
+		break;
+	    case 6:
 		printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_7.png"));
 	    }
-	
-	if (hangman.getError() == hangman.getMaxErrors()) {
-	    printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_7.png"));
-	    print.setText("You lose The word was: " + hangmanwordguess.getWord());
-	    teller++;
-	} else if(hangmanwordguess.getWord().equals(geussedWord.getText())){
-	    print.setText("You win");
-	    teller++;
+
+	    if (hangman.getError() == hangman.getMaxErrors()) {
+		print.setText("You lose The word was: " + hangmanwordguess.getWord());
+		teller++;
+	    } else if (hangmanwordguess.getWord().equals(geussedWord.getText())) {
+		print.setText("You win");
+		teller++;
+	    } else {
+		print.setText("");
+	    }
+	} else {
+
 	}
-	else {
-	    print.setText("");
-	}
-	    	}
-	    	else {
-	    	    
-	    	}
+    }
+
+    public void stop() {
+	teller++;
+	restart.setVisible(true);
     }
     
-    public void Stop() {
-	System.exit(0);
+    public void restart() {
+	teller = 0;
+	hangman.setError(0);
+	printGallow.setImage(new Image("eu/deltasource/hangman/foto/galg_1.png"));
+	hangmanwordguess = new HangmanWordGuess();
+	geussedWord.setText(hangmanwordguess.getGuessedWord());
+	availableChar.setText(hangmanwordguess.getAvailableCharacters());
+	restart.setVisible(false);
     }
 }
