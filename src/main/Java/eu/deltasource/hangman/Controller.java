e6d1ac574d2b14ad6a34df1ea93750a0d724da15
mverbids <maximeverbinnen@deltasource.eu>
2024-02-08 15:06:00 +0100

    [refactor]: fixed indentation and unused import statements

--- src/main/Java/eu/deltasource/hangman/Controller.java
+++ src/main/Java/eu/deltasource/hangman/Controller.java
@@ -19,66 +19,66 @@ public class Controller {
     public int teller = 0;
 
     public void initialize() {
-	hangman = new Hangman();
-	hangmanwordguess = new HangmanWordGuess();
-	printGallow.setImage(new Image(gallow(1)));
-	geussedWord.setText(hangmanwordguess.getGuessedWord());
-	availableChar.setText(hangmanwordguess.getAvailableCharacters());
-	restart.setVisible(false);
+        hangman = new Hangman();
+        hangmanwordguess = new HangmanWordGuess();
+        printGallow.setImage(new Image(gallow(1)));
+        geussedWord.setText(hangmanwordguess.getGuessedWord());
+        availableChar.setText(hangmanwordguess.getAvailableCharacters());
+        restart.setVisible(false);
     }
 
     public String gallow(int gallow) {
-	return "/img/galg_" + gallow + ".png";
+        return "/img/galg_" + gallow + ".png";
     }
 
     public void guess() {
-	Character character = null;
-	if (teller == 0) {
+        Character character = null;
+        if (teller == 0) {
 
-	    if (letter.getText().length() == 1) {
-		character = letter.getText().charAt(0);
-	    } else {
-		print.setText("only one letter please");
-	    }
+            if (letter.getText().length() == 1) {
+                character = letter.getText().charAt(0);
+            } else {
+                print.setText("only one letter please");
+            }
 
-	    if (hangmanwordguess.guessCharacter(character) == false) {
-		hangman.setError(hangman.getError() + 1);
-	    }
+            if (!hangmanwordguess.guessCharacter(character)) {
+                hangman.setError(hangman.getError() + 1);
+            }
 
-	    availableChar.setText(hangmanwordguess.getAvailableCharacters());
-	    geussedWord.setText(hangmanwordguess.getGuessedWord());
-	    letter.clear();
+            availableChar.setText(hangmanwordguess.getAvailableCharacters());
+            geussedWord.setText(hangmanwordguess.getGuessedWord());
+            letter.clear();
 
-	    printGallow.setImage(new Image(gallow(hangman.getError()+1)));
+            printGallow.setImage(new Image(gallow(hangman.getError() + 1)));
 
-	    if (hangman.getError() == hangman.getMaxErrors()) {
-		print.setText("You lose The word was: " + hangmanwordguess.getWord());
-		teller++;
-		restart.setVisible(true);
-	    } else if (hangmanwordguess.getWord().equals(geussedWord.getText())) {
-		print.setText("You win");
-		teller++;
-		restart.setVisible(true);
-	    } else {
-		print.setText("");
-	    }
-	}
+            if (hangman.getError() == hangman.getMaxErrors()) {
+                print.setText("You lose The word was: " + hangmanwordguess.getWord());
+                teller++;
+                restart.setVisible(true);
+            } else if (hangmanwordguess.getWord().equals(geussedWord.getText())) {
+                print.setText("You win");
+                teller++;
+                restart.setVisible(true);
+            } else {
+                print.setText("");
+            }
+        }
     }
 
     public void stop() {
-	teller++;
-	restart.setVisible(true);
-	print.setText("You lose The word was: " + hangmanwordguess.getWord());
+        teller++;
+        restart.setVisible(true);
+        print.setText("You lose The word was: " + hangmanwordguess.getWord());
     }
 
     public void restart() {
-	teller = 0;
-	hangman.setError(0);
-	printGallow.setImage(new Image(gallow(1)));
-	hangmanwordguess = new HangmanWordGuess();
-	geussedWord.setText(hangmanwordguess.getGuessedWord());
-	availableChar.setText(hangmanwordguess.getAvailableCharacters());
-	print.setText("");
-	restart.setVisible(false);
+        teller = 0;
+        hangman.setError(0);
+        printGallow.setImage(new Image(gallow(1)));
+        hangmanwordguess = new HangmanWordGuess();
+        geussedWord.setText(hangmanwordguess.getGuessedWord());
+        availableChar.setText(hangmanwordguess.getAvailableCharacters());
+        print.setText("");
+        restart.setVisible(false);
     }
 }
